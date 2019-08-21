05c818950f3253debfc883eeb1a41968ea591c62
seapp88 <seapp88@icloud.com>
2019-08-21 06:38:53 +0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,20 @@
+const mosca = require('mosca');
+
+const ascoltatore = {
+    //using ascoltatore
+    type: 'mongo',
+    url: 'mongodb://localhost:27017/mqtt1',
+    pubsubCollection: 'ascoltatori',
+    mongo: {}
+};
+
+const settings = {
+    port: 1884,
+    backend: ascoltatore
+};
+
+const server = new mosca.Server(settings);
+
+server.on('clientConnected', (client) => {
+    console.log('client connected', client.id);
+});

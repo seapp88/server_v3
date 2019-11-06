370743021090cc694c04710d2a3822aa2b5ee1b9
seapp88 <seapp88@icloud.com>
2019-11-06 23:26:44 +0300

    g

--- workers/usb.js
+++ workers/usb.js
@@ -2,6 +2,6 @@ const drivelist = require('drivelist');
 
 setInterval(async () => {
     const drives = await drivelist.list();
-    let res = drives.filter(x => x.busType === 'USB' && x.mountpoints[0].path);
+    let res = drives.filter(x => x.busType === 'USB' && x.mountpoints[0] && x.mountpoints[0].path);
     process.send(res);
 }, 5000);
